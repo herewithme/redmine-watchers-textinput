30eb25264e8f8748351ff9fc27ef773167bf4990
Amaury Balmer <amaury@beapi.fr>
2014-04-13 23:20:11 +0200

    change plugin name

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ require 'redmine'
 require_dependency 'redmine_watchers_textinput/hooks'
 
 Redmine::Plugin.register :redmine_watchers_textinput do
-  name 'Watchers Textinput plugin'
+  name 'Redmine Watchers Textinput'
   author 'Amaury Balmer'
   description 'Redmine plugin to convert watchers checkboxes to more compact autocomplete text field'
   version '0.2'
